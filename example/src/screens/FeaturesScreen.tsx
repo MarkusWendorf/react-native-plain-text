45523074353190ad267e815d56b64e8b179d6e5d
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2026-09-21 21:31:11 +0200

    feat(android): `android_hyphenationFrequency` prop (#31)
    Co-Authored-By: Markus Wendorf <markuswendorf@web.de>

--- example/src/screens/FeaturesScreen.tsx
+++ example/src/screens/FeaturesScreen.tsx
@@ -19,6 +19,7 @@ import { FontStyleSection } from '../sections/FontStyleSection';
 import { FontVariantSection } from '../sections/FontVariantSection';
 import { FontVariationSettingsSection } from '../sections/FontVariationSettingsSection';
 import { FontWeightSection } from '../sections/FontWeightSection';
+import { HyphenationSection } from '../sections/HyphenationSection';
 import { LetterSpacingSection } from '../sections/LetterSpacingSection';
 import { LineBreakStrategySection } from '../sections/LineBreakStrategySection';
 import { LineHeightClippingSection } from '../sections/LineHeightClippingSection';
@@ -76,6 +77,7 @@ export default function FeaturesScreen({ navigation }: Props) {
     ['Ellipsize Mode', EllipsizeModeSection],
     ['Line Break Strategy', LineBreakStrategySection],
     ['Text Break Strategy', TextBreakStrategySection],
+    ['Hyphenation', HyphenationSection],
     ['Text Decoration Line', TextDecorationLineSection],
     ['Text Shadow', TextShadowSection],
     ['Text Transform', TextTransformSection],
